a817a4e4fcae23856d0f3df2dbf0499d21acf64c
Анна Жжонова <annzhzho@gmail.com>
2023-09-19 19:51:47 +0400

    change colours

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import 'normalize.css';
 import './assets/styles/App.css';
 import Header from './components/Header/Header.jsx';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer';
+import WordsTable from './components/WordsTable/WordsTable';
+
+import data from './wordsList.json';
+
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Main></Main>
+      {data.map((item) =>
+      <WordsTable
+      eng={item.english}
+      transcription={item.transcription}
+      rus={item.russian}
+      ></WordsTable>
+      )}
       <Footer></Footer>
     </div>
   );
--- src/components/WordsTable/WordsTable.jsx
+++ src/components/WordsTable/WordsTable.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function WordsTable(props) {
     const {eng,transcription, rus} = props;
   return (
-    <React.Component>
+    <div>
         <div>{eng}</div>
         <div>{transcription}</div>
         <div>{rus}</div>
-    </React.Component>
+    </div>
   )
 }
